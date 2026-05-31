9976f9d3011c487691fe3b2939d28f1b0104065a
Jan Gabriel da Silva Gomes <qualidade@iamccongelados.com.br>
2026-05-31 08:14:43 -0300

    Atualizacao do sistema (via Script Automatico)

---
+++ atualizar.bat
@@ -0,0 +1,30 @@
+@echo off
+chcp 65001 >nul
+echo ===============================================
+echo     ATUALIZADOR AUTOMÁTICO DO SISTEMA (GITHUB)
+echo ===============================================
+echo.
+
+IF NOT EXIST ".git" (
+    echo [!] Configurando a pasta para o Github pela primeira vez...
+    git init
+    git remote add origin https://github.com/jangabrielxg-bit/manutencao.git
+    git branch -M main
+)
+
+echo Adicionando arquivos alterados...
+git add .
+
+echo.
+echo Criando um registro da alteracao...
+git commit -m "Atualizacao do sistema (via Script Automatico)"
+
+echo.
+echo Enviando para a nuvem do Github...
+git push -u origin main
+
+echo.
+echo ===============================================
+echo    Pronto! Seu site ja esta atualizado!
+echo ===============================================
+pause
